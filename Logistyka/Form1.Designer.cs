e50c4b29894307c623fa96c1937634c7736dcaaa
mpalucki <mat.palucki@gmail.com>
2019-03-19 00:27:22 +0100

    added Gantt chart

--- Logistyka/Form1.Designer.cs
+++ Logistyka/Form1.Designer.cs
@@ -45,6 +45,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // lb1
@@ -190,11 +192,36 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 12;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 318);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "rysuj wykres Gantta";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 99.10714F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.8928571F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 372);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.941176F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 97.05882F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(797, 168);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1069, 648);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -234,6 +261,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+        internal System.Windows.Forms.ContextMenuStrip ContextMenuGanttChart1;
+        internal System.Windows.Forms.ToolStripMenuItem SaveImageToolStripMenuItem;
     }
 }
 
--- Logistyka/Form1.cs
+++ Logistyka/Form1.cs
@@ -13,6 +13,11 @@ namespace Logistyka
 {
     public partial class Form1 : Form
     {
+        TextBox txtLog;
+        GanttChart ganttChart1;
+
+
+
         public List<Activity> activities;
         public Form1()
         {
@@ -83,5 +88,78 @@ namespace Logistyka
         {
             textBox1.Text = activities.ToList().First().Duration;
         }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+            txtLog = new TextBox();
+            txtLog.Dock = DockStyle.Fill;
+            txtLog.Multiline = true;
+            txtLog.Enabled = false;
+            txtLog.ScrollBars = ScrollBars.Horizontal;
+            tableLayoutPanel1.Controls.Add(txtLog, 0, 3);
+
+            //first Gantt Chart
+            ganttChart1 = new GanttChart();
+            ganttChart1.AllowChange = false;
+            ganttChart1.Dock = DockStyle.Fill;
+            ganttChart1.FromDate = new DateTime(2015, 12, 12, 0, 0, 0);
+            ganttChart1.ToDate = new DateTime(2015, 12, 24, 0, 0, 0);
+            tableLayoutPanel1.Controls.Add(ganttChart1, 0, 1);
+
+            ganttChart1.MouseMove += new MouseEventHandler(ganttChart1.GanttChart_MouseMove);
+            ganttChart1.MouseMove += new MouseEventHandler(GanttChart1_MouseMove);
+            ganttChart1.MouseDragged += new MouseEventHandler(ganttChart1.GanttChart_MouseDragged);
+            ganttChart1.MouseLeave += new EventHandler(ganttChart1.GanttChart_MouseLeave);
+            ganttChart1.ContextMenuStrip = ContextMenuGanttChart1;
+
+            List<BarInformation> lst1 = new List<BarInformation>();
+
+            foreach(Activity act in activities)
+            {
+                lst1.Add(new BarInformation());
+            }
+
+
+            lst1.Add(new BarInformation("Row 1", new DateTime(2015, 12, 12), new DateTime(2015, 12, 16), Color.Aqua, Color.Khaki, 0));
+            lst1.Add(new BarInformation("Row 2", new DateTime(2015, 12, 13), new DateTime(2015, 12, 20), Color.AliceBlue, Color.Khaki, 1));
+            lst1.Add(new BarInformation("Row 3", new DateTime(2015, 12, 14), new DateTime(2015, 12, 24), Color.Violet, Color.Khaki, 2));
+            lst1.Add(new BarInformation("Row 2", new DateTime(2015, 12, 21), new DateTime(2015, 12, 22, 12, 0, 0), Color.Yellow, Color.Khaki, 1));
+            lst1.Add(new BarInformation("Row 1", new DateTime(2015, 12, 17), new DateTime(2015, 12, 24), Color.LawnGreen, Color.Khaki, 0));
+
+            foreach (BarInformation bar in lst1)
+            {
+                ganttChart1.AddChartBar(bar.RowText, bar, bar.FromTime, bar.ToTime, bar.Color, bar.HoverColor, bar.Index);
+            }
+
+        }
+
+        private void GanttChart1_MouseMove(Object sender, MouseEventArgs e)
+        {
+            List<string> toolTipText = new List<string>();
+
+            if (ganttChart1.MouseOverRowText.Length > 0)
+            {
+                BarInformation val = (BarInformation)ganttChart1.MouseOverRowValue;
+                toolTipText.Add("[b]Date:");
+                toolTipText.Add("From ");
+                toolTipText.Add(val.FromTime.ToLongDateString() + " - " + val.FromTime.ToString("HH:mm"));
+                toolTipText.Add("To ");
+                toolTipText.Add(val.ToTime.ToLongDateString() + " - " + val.ToTime.ToString("HH:mm"));
+            }
+            else
+            {
+                toolTipText.Add("");
+            }
+
+            ganttChart1.ToolTipTextTitle = ganttChart1.MouseOverRowText;
+            ganttChart1.ToolTipText = toolTipText;
+
+        }
+
+
     }
 }
